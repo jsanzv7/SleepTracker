74106472425094525f1364696c3072c1ce3c68ad
jsanz <jsanzv7@gmail.com>
2022-10-19 15:57:07 -0400

    added adapter

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -61,6 +61,17 @@ class SleepTrackerFragment : Fragment() {
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
 
+
+        //JS Why is it placed here
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer{
+            it?.let {
+                adapter.data = it
+            }
+        })
+
         // binding.setLifecycleOwner(this)
         binding.lifecycleOwner = this
 
